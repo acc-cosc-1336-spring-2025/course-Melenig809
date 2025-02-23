61c8243338acd5e29b2b8e02272cc08e341996a9
Melenig809 <meleni.gonzalez@g.austincc.edu>
2025-02-23 20:17:59 +0000

    HW1 - MG

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,8 @@
 #
+import output
+
+def main():
+    result = output.multiply_numbers(5, 10)
+    print(result)
+
+main()
--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -4,7 +4,7 @@ import unittest
 The file at /src/homework/b_in_proc_out/output has 
 the function get_number.
 '''
-from src.homework.b_in_proc_out.output import get_number
+from src.homework.b_in_proc_out.output import get_number, multiply_numbers
 
 class Test_Config(unittest.TestCase):
 
@@ -15,4 +15,9 @@ class Test_Config(unittest.TestCase):
     def test_get_number_2(self):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
+
+    def test_multipy_numbers(self):
+        self.assertEqual(multiply_numbers(5, 10), 50)
+        self.assertEqual(multiply_numbers(3, 2), 6)
+        self.assertEqual(multiply_numbers(4, 5), 20)
 
